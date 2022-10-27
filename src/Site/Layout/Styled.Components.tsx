05d40ea8f9c420ccae0ed9cbf2f318fe12d462c7
Shivangam Soni <shivangamsoni99@gmail.com>
2022-10-27 10:29:46 +0530

    Update: Layout Style

--- src/Site/Layout/Styled.Components.tsx
+++ src/Site/Layout/Styled.Components.tsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    width: 100vw;
-    height: 100vh;
+    min-height: 100vh;
 
     background-image: linear-gradient(
         to bottom,
